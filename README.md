1439aaba70dd7dff3832f457842330b4668e2fbe
Matthew Hart <mattevanhart@gmail.com>
2023-07-03 17:31:39 -0400

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+a wrapper around nodemailer to send emails with env vars and a single function call
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * A function that takes in a single recipient or an array of recipients
+ *  and sends an email to each of them with the optional message
+ *
+ * @param {string | string[]}     recipients - A single recipient or array of recipients
+ * @param {string}                [message]  - An optional message to send to the recipients
+ *
+ */
+function sendEmail(recipients: string | string[], message?: string) {
+	if (message) console.log("Message is ", message);
+	if (!Array.isArray(recipients)) recipients = [recipients];
+	recipients.forEach((recipient) =>
+		console.log("Sending email to ", recipient)
+	);
+	return;
+}
+
+export default sendEmail;
